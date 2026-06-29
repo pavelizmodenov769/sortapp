1fbfd2ca75253f1af914de543fac5ee29f6dd0f3
Zuev_Sergey <zuevkajo@gmail.com>
2026-06-29 12:12:52 +0300

    add class Student + Builder

---
+++ src/main/java/model/Student.java
@@ -0,0 +1,68 @@
+package model;
+
+import java.util.Objects;
+
+public class Student {
+    private final String group;
+    private final double avarageScore;
+    private final String creditBook;
+
+    private Student(Builder builder) {
+        this.group = builder.group;
+        this.avarageScore = builder.avarageScore;
+        this.creditBook = builder.creditBook;
+    }
+
+    public String getGroup() {
+        return group;
+    }
+
+    public double getAvarageScore() {
+        return avarageScore;
+    }
+
+    public String getCreditBook() {
+        return creditBook;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return Double.compare(avarageScore, student.avarageScore) == 0 &&
+                Objects.equals(group, student.group) &&
+                Objects.equals(creditBook, student.creditBook);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(group, avarageScore, creditBook);
+    }
+
+    // Builder for Student
+    public static class Builder {
+        private String group;
+        private double avarageScore;
+        private String creditBook;
+
+        public Builder setGroup(String group) {
+            this.group = group;
+            return this;
+        }
+
+        public Builder setAvaregeScore(double avaregeScore) {
+            this.avarageScore = avaregeScore;
+            return this;
+        }
+
+        public Builder setCreditBook(String creditBook) {
+            this.creditBook = creditBook;
+            return this;
+        }
+
+        public Student build() {
+            return new Student(this);
+        }
+    }
+}
