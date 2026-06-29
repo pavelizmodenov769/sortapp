d110151a324f2eb2b5df980e1095653530e1ea13
Zuev_Sergey <zuevkajo@gmail.com>
2026-06-29 17:17:14 +0300

    add StudentManualTest

---
+++ src/test/java/com/team/sortapp/manual/StudentManualTest.java
@@ -0,0 +1,40 @@
+package com.team.sortapp.manual;
+
+import model.Student;
+
+public class StudentManualTest {
+
+    public static void main(String[] args) {
+        Student student1 = new Student.Builder()
+                .setGroup("JAVA-102")
+                .setAvaregeScore(4.8)
+                .setCreditBook("AA 102")
+                .build();
+
+        Student student2 = new Student.Builder()
+                .setGroup("JAVA-102")
+                .setAvaregeScore(4.8)
+                .setCreditBook("AA 102")
+                .build();
+
+        Student student3 = new Student.Builder()
+                .setGroup("JAVA-103")
+                .setAvaregeScore(4.7)
+                .setCreditBook("TT 103")
+                .build();
+
+        System.out.println("Студент1: " + student1);
+        System.out.println("Студент2: " + student2);
+        System.out.println("Студент3: " + student3);
+        System.out.println();
+
+        System.out.println("Equals: " + student1.equals(student2));
+        System.out.println("Equals: " + student1.equals(student3));
+        System.out.println();
+
+        System.out.println("HashCodes:");
+        System.out.println(student1.hashCode());
+        System.out.println(student2.hashCode());
+        System.out.println(student3.hashCode());
+    }
+}
